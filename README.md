e28abc110ccc2e1295dda54a197cfcfeea0f52b0
max <max@foo.bar>
2016-02-10 11:37:23 +0100

    Streamed replication support

--- README.md
+++ README.md
@@ -1,24 +1,68 @@
-Database container
-==
+# PostgreSQL docker image based on Alpine Linux with streamed replication support
 
-Forked and tweaked from [kiasaki/docker-alpine-postgres](//github.com/kiasaki/docker-alpine-postgres)
+This repo builds a docker image that accepts the same env vars as the
+[official postgres build](https://registry.hub.docker.com/_/postgres/) but
+with a much smaller footprint. It achieves that by basing itself off the great
+[alpine](https://github.com/gliderlabs/docker-alpine) docker image by GliderLabs.
 
-Build
---
-
-```bash
-$ docker build -t quay.io/cluster_mailboxes/docker-alpine-postgres:latest .
-$ docker push quay.io/cluster_mailboxes/docker-alpine-postgres:latest
-```
-
-Running
---
+## Why?
 
 ```bash
-$ docker run -iP -e POSTGRES_PASSWORD='something' -e POSTGRES_USER='bilbo' -e POSTGRES_DB='someshite' quay.io/cluster_mailboxes/docker-alpine-postgres:latest
+$ docker images
+REPOSITORY          TAG         VIRTUAL SIZE
+alpine-postgres     latest      22.28 MB
+postgres            latest      213.90 MB
 ```
 
-License
---
+# Build
+
+```bash
+$ make build
+```
+
+# Usage
+
+This image works in the same way the official `postgres` docker image work.
+
+It's documented on DockerHub in it's README: [https://hub.docker.com/_/postgres/](https://hub.docker.com/_/postgres/).
+
+For example, you can start a basic PostgreSQL server, protected by a password,
+listening on port 5432 by running the following:
+
+```
+$ docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d alpine-postgres
+```
+
+Next, you can start you app's container while **linking** it to the PostgreSQL
+container you just created giving it access to it.
+
+```
+$ docker run --name some-app --link some-postgres:postgres -d application-that-uses-postgres
+```
+
+Your app will now be able to access `POSTGRES_PORT_5432_TCP_ADDR` and `POSTGRES_PORT_5432_TCP_PORT` environment variables.
+
+# Streamed replication
+
+Since postgres needs as much HDD IO speed as possible, network storage solutions can be a big bottleneck. Using host directories is
+a fast solution. The easiest way to add redundancy in such a situation is to use streamed replication.
+
+## Master setup
+
+To enable replication on a new master installation, set `POSTGRES_REPLICATION` to `master`.
+By default `POSTGRES_REPLICATION_USER` is set to `replication` and `POSTGRES_REPLICATION_PASSWORD` is set to `POSTGRES_PASSWORD`.
+
+The config variables `wal_level`, `max_wal_senders` and `wal_keep_segments` are modified in the new `postgresql.conf` config file.
+
+## Client setup
+
+Set `POSTGRES_REPLICATION` to `slave` and `POSTGRES_REPLICATION_MASTER_HOST` to the hostname/IP of the current master server.
+`POSTGRES_REPLICATION_MASTER_PORT` defaults to 5432 which should fit in most situations.
+
+The `PGDATA` directory have to be empty to start the initial replication. The replication is done via the tool `pg_basebackup`.
+
+Note that the initial backup can take some time, depending on the database size.
+
+# License
 
 MIT. See `LICENSE` file.
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,52 +2,79 @@
 set -e
 
 chown -R postgres "$PGDATA"
+chmod 0700 "$PGDATA"
 
 # Bomb out if no password set
 : ${POSTGRES_PASSWORD:?}
+: ${POSTGRES_REPLICATION:="no"}
+: ${POSTGRES_REPLICATION_USER:="replication"}
+: ${POSTGRES_REPLICATION_PASSWORD:="$POSTGRES_PASSWORD"}
 
 if [ -z "$(ls -A "$PGDATA")" ]; then
-    gosu postgres initdb
-    sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
-
-    : ${POSTGRES_USER:="postgres"}
-    : ${POSTGRES_DB:=$POSTGRES_USER}
-
-    if [ "$POSTGRES_DB" != 'postgres' ]; then
-      createSql="CREATE DATABASE $POSTGRES_DB;"
-      echo $createSql | gosu postgres postgres --single -jE
-      echo
-    fi
-
-    if [ "$POSTGRES_USER" != 'postgres' ]; then
-      op=CREATE
-    else
-      op=ALTER
-    fi
-
-    userSql="$op USER $POSTGRES_USER WITH SUPERUSER PASSWORD '${POSTGRES_PASSWORD}';"
-    echo $userSql | gosu postgres postgres --single -jE
-    echo
-
-    # internal start of server in order to allow set-up using psql-client
-    # does not listen on TCP/IP and waits until start finishes
-    gosu postgres pg_ctl -D "$PGDATA" \
-        -o "-c listen_addresses=''" \
-        -w start
-
-    echo
-    for f in /docker-entrypoint-initdb.d/*; do
-        case "$f" in
-            *.sh)  echo "$0: running $f"; . "$f" ;;
-            *.sql) echo "$0: running $f"; psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < "$f" && echo ;;
-            *)     echo "$0: ignoring $f" ;;
-        esac
+    if [ "$POSTGRES_REPLICATION" = "slave" ] ; then
+        : ${POSTGRES_REPLICATION_MASTER_HOST:?}
+        : ${POSTGRES_REPLICATION_MASTER_PORT:=5432}
+        echo "$POSTGRES_REPLICATION_MASTER_HOST:$POSTGRES_REPLICATION_MASTER_PORT:replication:$POSTGRES_REPLICATION_USER:$POSTGRES_REPLICATION_PASSWORD" > /var/lib/postgresql/.pgpass
+        chown postgres /var/lib/postgresql/.pgpass
+        chmod 0600 /var/lib/postgresql/.pgpass
+        gosu postgres pg_basebackup -h $POSTGRES_REPLICATION_MASTER_HOST -p $POSTGRES_REPLICATION_MASTER_PORT -D "$PGDATA" -R -P -v -w -U $POSTGRES_REPLICATION_USER --xlog-method=stream
+        rm /var/lib/postgresql/.pgpass
         echo
-    done
+    else
+        gosu postgres initdb
+        sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
 
-    gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
+        : ${POSTGRES_USER:="postgres"}
+        : ${POSTGRES_DB:=$POSTGRES_USER}
 
-    { echo; echo "host all all 0.0.0.0/0 md5"; } >> "$PGDATA"/pg_hba.conf
+        if [ "$POSTGRES_DB" != 'postgres' ]; then
+          createSql="CREATE DATABASE $POSTGRES_DB;"
+          echo $createSql | gosu postgres postgres --single -jE
+          echo
+        fi
+
+        if [ "$POSTGRES_USER" != 'postgres' ]; then
+          op=CREATE
+        else
+          op=ALTER
+        fi
+
+        userSql="$op USER $POSTGRES_USER WITH SUPERUSER PASSWORD '${POSTGRES_PASSWORD}';"
+        echo $userSql | gosu postgres postgres --single -jE
+        echo
+
+        if [ "$POSTGRES_REPLICATION" = "master" ] ; then
+            sed -ri "s/^#(wal_level\s*=\s*)\S+/\1hot_standby/" "$PGDATA"/postgresql.conf
+            sed -ri "s/^#(max_wal_senders\s*=\s*)\S+/\15/" "$PGDATA"/postgresql.conf
+            sed -ri "s/^#(wal_keep_segments\s*=\s*)\S+/\132/" "$PGDATA"/postgresql.conf
+            userSql="$op USER $POSTGRES_REPLICATION_USER WITH REPLICATION PASSWORD '${POSTGRES_REPLICATION_PASSWORD}';"
+            echo $userSql | gosu postgres postgres --single -jE
+            echo
+        fi
+
+        # internal start of server in order to allow set-up using psql-client
+        # does not listen on TCP/IP and waits until start finishes
+        gosu postgres pg_ctl -D "$PGDATA" \
+            -o "-c listen_addresses=''" \
+            -w start
+
+        echo
+        for f in /docker-entrypoint-initdb.d/*; do
+            case "$f" in
+                *.sh)  echo "$0: running $f"; . "$f" ;;
+                *.sql) echo "$0: running $f"; psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < "$f" && echo ;;
+                *)     echo "$0: ignoring $f" ;;
+            esac
+            echo
+        done
+
+        gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
+
+        { echo; echo "host all all 0.0.0.0/0 md5"; } >> "$PGDATA"/pg_hba.conf
+        if [ "$POSTGRES_REPLICATION" = "master" ] ; then
+            { echo; echo "host replication $POSTGRES_REPLICATION_USER 0.0.0.0/0 md5"; } >> "$PGDATA"/pg_hba.conf
+        fi
+    fi
 fi
 
 echo 'Starting database'
